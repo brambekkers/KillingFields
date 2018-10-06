841c0401714bb878ca52a25838b696ac763255bf
Bram Bekkers <bram.bekkers@outlook.com>
2018-10-06 16:18:21 +0200

    laatste wijzigingen

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// Laad html
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+// User handler
 io.on('connection', function(socket){
+
+  // connection
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 
+  // afhandelen berichten
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
